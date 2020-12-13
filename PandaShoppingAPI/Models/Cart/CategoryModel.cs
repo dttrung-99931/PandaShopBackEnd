e00f836456f918f907411215aae4a72e8097e2bf
Đoàn Thanh Trung <doanthanhtrung4321@gmail.com>
2020-12-13 22:59:53 +0700

    Switch to local

---
+++ PandaShoppingAPI/Models/Cart/CategoryModel.cs
@@ -0,0 +1,21 @@
+﻿using PandaShoppingAPI.DataAccesses.EF;
+using PandaShoppingAPI.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PandaShoppingAPI.Models
+{
+    public class CategoryModel: BaseModel<Category, CategoryModel>
+    {
+        public string name { get; set; }
+        public int? parentId { get; set; }
+        public string based64Img { get; set; }
+        public int level { get; set; }
+
+        [JsonIgnore]
+        public int imageId { get; set; }
+    }
+}
---
+++ PandaShoppingAPI/Models/Cart/CategoryResponseModel.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using PandaShoppingAPI.DataAccesses.EF;
+using PandaShoppingAPI.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PandaShoppingAPI.Models
+{
+    public class CategoryResponse : BaseModel<Category, CategoryResponse>
+    {
+        public string name { get; set; }
+        public string imgLink { get; set; }
+        public int? templateId { get; set; }
+        public int level { get; set; }
+
+        protected override void CustomMapping(
+            IMappingExpression<Category, CategoryResponse> mappingExpression, 
+            IConfiguration config)
+        {
+            mappingExpression.ForMember
+            (
+                responseCategory => responseCategory.imgLink,
+                option => option.MapFrom(
+                    category => category.imageId != null 
+                        ? Path.Combine(
+                            config["Path:CategoryImgEndPoint"], 
+                            category.image.fileName
+                        ) 
+                        : null
+                )
+            );
+        }
+    }
+}
