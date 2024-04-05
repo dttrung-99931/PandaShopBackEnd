cb75c8fa572704ba71483ec65e62ce2aff4ff3a6
ptt <ptt99931.work@gmail.com>
2024-04-05 23:57:12 +0700

    [Entities] fix wrong ProductBatch foreign key

--- PandaShoppingAPI/DataAccesses/EF/WarehouseInput.cs
+++ PandaShoppingAPI/DataAccesses/EF/WarehouseInput.cs
@@ -5,10 +5,16 @@ namespace PandaShoppingAPI.DataAccesses.EF
 {
     public partial class WarehouseInput
     {
+        public WarehouseInput()
+        {
+            ProductBatch = new HashSet<ProductBatch>();
+        }
+
         public int id { get; set; }
         public int warehouseId { get; set; }
         public DateTime date { get; set; }
 
         public virtual Warehouse warehouse { get; set; }
+        public virtual ICollection<ProductBatch> ProductBatch { get; set; }
     }
 }
