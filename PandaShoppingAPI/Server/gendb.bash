445c30df14425ce0b81af3a85d7ab4d44bbc8491
ptt <ptt99931.work@gmail.com>
2024-04-14 07:50:55 +0700

    [server] add gendb scripts

---
+++ PandaShoppingAPI/Server/gendb.bash
@@ -0,0 +1,5 @@
+﻿// Home 
+dotnet ef dbcontext scaffold "Server=localhost,1433;Database=PandaShopDB;User Id=sa;Password=0988202071aA@;"  Microsoft.EntityFrameworkCore.SqlServer -o DataAccesses/EF -c  EcommerceDBContext --use-database-names --no-pluralize --no-onconfiguring -f
+
+// Company
+dotnet ef dbcontext scaffold "Server=.\SQLEXPRESS;Initial Catalog=PandaShopDB;Integrated Security=True"  Microsoft.EntityFrameworkCore.SqlServer -o DataAccesses/EF -c  EcommerceDBContext --use-database-names --no-pluralize --no-onconfiguring -f
