3b22156b1e0c1e644485c5b5ebbbbfad22a446b6
ptt <ptt99931.work@gmail.com>
2024-04-15 21:54:29 +0700

    [order] handle output inventory after orders created

---
+++ PandaShoppingAPI/Models/Order/AvailableProductOptionRespone.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PandaShoppingAPI.Models.Order
+{
+	public class AvailableProductOptionRespone
+	{
+		public int productOptionId { set; get; }	
+		public int remainingNumber { set; get; }
+	}
+}
+
