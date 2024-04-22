698ba3578471ad22a4bc21ba519b97c7b326b0ef
ptt <ptt99931.work@gmail.com>
2024-04-23 00:46:45 +0700

    [order] add StartProcessOrder APIs, ... using partial class order

---
+++ PandaShoppingAPI/Services/Order/OrderProcess/OrderProcessService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PandaShoppingAPI.DataAccesses.EF;
+using PandaShoppingAPI.DataAccesses.Repos;
+using PandaShoppingAPI.Models;
+using PandaShoppingAPI.Models.Order;
+using PandaShoppingAPI.Utils;
+using PandaShoppingAPI.Utils.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PandaShoppingAPI.Services
+{
+    public partial class OrderService
+    {
+        public void CompleteProcessingOrder(int orderId)
+        {
+            UpdateOrderStatus(orderId, OrderStatus.WaitingForDelivering);
+        }
+
+        public void StartProcessingOrder(int orderId)
+        {
+            UpdateOrderStatus(orderId, OrderStatus.Processing);
+        }
+
+        private void UpdateOrderStatus(int orderId, OrderStatus status)
+        {
+            Order_ order = _repo.GetById(orderId);
+            order.status = status;
+            _repo.Update(order, order.id);
+        }
+    }
+}
