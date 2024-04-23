88b07cfd3c1ee2e66c2d3e730db0ba5f165f1aad
Đoàn Thanh Trung <doanthanhtrung4321@gmail.com>
2024-04-23 14:33:59 +0700

    [data access] Update Cart table - add default isDeleted

---
+++ PandaShoppingAPI/Migrations/20240423071550_Update_Cart_Set-default-isDeleted.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PandaShoppingAPI.Migrations
+{
+    public partial class Update_Cart_SetdefaultisDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isDeleted",
+                table: "Cart",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isDeleted",
+                table: "Cart",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+        }
+    }
+}
