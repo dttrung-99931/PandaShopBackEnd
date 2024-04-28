aedc7a9c36d021518f9e6d61fde2469de9cd7fd5
ptt <ptt99931.work@gmail.com>
2024-04-28 13:54:24 +0700

    [product] fix update product, [data access] addQeuryFilter isDeleted, db inteceptor

---
+++ PandaShoppingAPI/DataAccesses/EF/Interceptor/SoftDeleteInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace PandaShoppingAPI.DataAccesses.EF.Interceptor
+{
+	public class SoftDeleteInterceptor: SaveChangesInterceptor
+	{
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            if (eventData.Context == null)
+            {
+                return result;
+            }
+
+            eventData.Context.ChangeTracker.Entries()
+                .Where((entry) => entry is { State: EntityState.Deleted, Entity: BaseEntity entity })
+                .ToList()
+                .ForEach((entry) =>
+                {
+                    entry.State = EntityState.Modified;
+                    (entry.Entity as BaseEntity).isDeleted = true;
+                });
+            return result;
+        }
+    }
+}
+
