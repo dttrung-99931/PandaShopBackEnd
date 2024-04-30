fcdbe4e2fd02bc79bd5d06c3ff5f1f31eb9cbaff
ptt <ptt99931.work@gmail.com>
2024-04-30 17:16:52 +0700

    [response wrapper] cammel case  meta fields

--- PandaShoppingAPI/Utils/Meta.cs
+++ PandaShoppingAPI/Utils/Meta.cs
@@ -8,24 +8,24 @@ namespace PandaShoppingAPI.Utils
         public const int MAX_PAGE_SIZE = 500;
 
         public int total { get; set; }
-        public int? page_size { get; set; }
-        public int? page_number { get; set; }
+        public int? pageSize { get; set; }
+        public int? pageNum { get; set; }
 
         private Meta(int total, int? page_size, int? page_number)
         {
             this.total = total;
-            this.page_size = page_size;
-            this.page_number = page_number;
+            this.pageSize = page_size;
+            this.pageNum = page_number;
             if (page_size != null && page_number != null
                 && page_size > 0 && page_number > 0 && page_size <= MAX_PAGE_SIZE)
             {
-                this.page_size = (int)page_size;
-                this.page_number = (int)page_number;
+                this.pageSize = (int)page_size;
+                this.pageNum = (int)page_number;
             }
             else
             {
-                this.page_size = 50;
-                this.page_number = 1;
+                this.pageSize = 50;
+                this.pageNum = 1;
             }
         }
 
