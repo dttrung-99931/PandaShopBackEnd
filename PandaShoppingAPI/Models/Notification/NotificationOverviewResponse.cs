eb08280c820c6f4b9b9325bb38f6804ba78e9326
ptt <ptt99931.work@gmail.com>
2024-05-01 21:49:33 +0700

    [noti] add get noti overview (new noti num, ..) api

---
+++ PandaShoppingAPI/Models/Notification/NotificationOverviewResponse.cs
@@ -0,0 +1,16 @@
+﻿using PandaShoppingAPI.DataAccesses.EF;
+using PandaShoppingAPI.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PandaShoppingAPI.Models
+{
+    public class NotificationOverviewResponse
+    {
+        public int newNotiNum { get; set; }
+        public int total { get; set; }
+    }
+}
