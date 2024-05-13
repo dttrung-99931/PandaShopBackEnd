75d6b16657bfc61f207b77b754b5f8807ce02438
Đoàn Thanh Trung <doanthanhtrung4321@gmail.com>
2024-05-13 17:26:08 +0700

    [address] add lat, long

---
+++ PandaShoppingAPI/DataAccesses/EF/Migrations/20240513101620_Update_Address_Add-long-lat.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PandaShoppingAPI.DataAccesses.EF.Migrations
+{
+    public partial class Update_Address_Addlonglat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "lat",
+                table: "Address",
+                type: "decimal(12,9)",
+                precision: 12,
+                scale: 9,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "long_",
+                table: "Address",
+                type: "decimal(12,9)",
+                precision: 12,
+                scale: 9,
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "lat",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "long_",
+                table: "Address");
+        }
+    }
+}
