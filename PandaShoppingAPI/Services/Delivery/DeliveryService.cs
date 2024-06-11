1563187059d45c91b7986a3f22d8c83b15040d3a
ptt <ptt99931.work@gmail.com>
2024-06-11 23:52:30 +0700

    [Order] delivery - fixing customerAddress

--- PandaShoppingAPI/Services/Delivery/DeliveryService.cs
+++ PandaShoppingAPI/Services/Delivery/DeliveryService.cs
@@ -1,4 +1,5 @@
-﻿using PandaShoppingAPI.DataAccesses.EF;
+﻿using AutoMapper;
+using PandaShoppingAPI.DataAccesses.EF;
 using PandaShoppingAPI.DataAccesses.Repos;
 using PandaShoppingAPI.Models;
 using System;
@@ -18,6 +19,21 @@ namespace PandaShoppingAPI.Services
         public override IQueryable<Delivery> Fill(DeliveryFilter filter)
         {
             return base.Fill(filter);
+        }
+
+        public void SetCustomerAddresses(List<DeliveryResponse> deliveries)
+        {
+            List<int> deliveryIds = deliveries.Select(delivery => delivery.id).ToList();
+            // Dic<deliveryId, aaddress≥
+            Dictionary<int, Address> customerAddresses = _repo.Where((delivery) => deliveryIds.Contains(delivery.id))
+                .ToList()
+                .ToDictionary(
+                    delivery => delivery.id,
+                    delivery => delivery.DeliveryLocation.First(location => location.locationType == LocationType.Delivery).address);
+        
+            foreach (DeliveryResponse delivery in deliveries){
+                delivery.customerAddress = Mapper.Map<AddressResponseModel>(customerAddresses[delivery.id]);
+            }
         }
     }
 }
