84b64f1209b38402e2e703ebdf81d1d3c629c20f
ptt <ptt99931.work@gmail.com>
2024-06-16 17:14:10 +0700

    [delivery] delivery - delivery locations

---
+++ PandaShoppingAPI/Models/Delivery/DeliveryLocationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using PandaShoppingAPI.DataAccesses.EF;
+using PandaShoppingAPI.Models.Base;
+
+namespace PandaShoppingAPI.Models
+{
+    public partial class DeliveryLocationResponse: BaseModel<DeliveryLocation, DeliveryLocationResponse>
+    {
+        public LocationType locationType { get; set; }
+        public virtual AddressResponseModel address { get; set; }
+    }
+}
